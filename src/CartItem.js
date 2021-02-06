26a89b237aaa5dab1bfd12fdc9d46133f576b138
Pradyuman Singh <66694735+pradyumansingh989@users.noreply.github.com>
2021-02-06 11:50:20 +0530

    Added increaseQuantity Func

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,6 +10,9 @@ class CartItem extends React.Component {
             img: ''
         }
     }
+    increaseQuantity = () => {
+        console.log('this',this.state);
+    }
     render() {
         const {price, title, qty} = this.state; //Object destructuring
         return (
@@ -23,9 +26,19 @@ class CartItem extends React.Component {
                     <div style={ { color: '#777' } }>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* {button} */}
-                        <img alt="increase" className="actions-icons" src="https://www.flaticon.com/svg/vstatic/svg/1828/1828926.svg?token=exp=1612591898~hmac=f3a7ae29f1c4c958832dd1a6d47875a2" />
-                        <img alt="decrease" className="actions-icons" src="https://www.flaticon.com/svg/vstatic/svg/1828/1828906.svg?token=exp=1612591934~hmac=630d323e02b12df8377fc2da2a1a5187" />
-                        <img alt="delete" className="actions-icons" src="https://www.flaticon.com/svg/vstatic/svg/1214/1214428.svg?token=exp=1612591986~hmac=0df38927315989ce404348ebfbdb8c05" />
+                        <img 
+                        alt="increase" className="actions-icons" 
+                        src="https://www.flaticon.com/svg/vstatic/svg/1828/1828926.svg?token=exp=1612591898~hmac=f3a7ae29f1c4c958832dd1a6d47875a2"
+                        onClick={this.increaseQuantity.bind(this)} 
+                         />
+
+                        <img 
+                        alt="decrease" className="actions-icons" 
+                        src="https://www.flaticon.com/svg/vstatic/svg/1828/1828906.svg?token=exp=1612591934~hmac=630d323e02b12df8377fc2da2a1a5187" />
+
+                        <img 
+                        alt="delete" className="actions-icons" 
+                        src="https://www.flaticon.com/svg/vstatic/svg/1214/1214428.svg?token=exp=1612591986~hmac=0df38927315989ce404348ebfbdb8c05" />
                     </div>
                 </div>
             </div>
